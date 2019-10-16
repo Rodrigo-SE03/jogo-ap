b4a1294e03de1c45c97fd1c31a85bc90f6b3344b
Rodrigo <sujeito300@gmail.com>
2019-10-16 13:55:21 -0300

    Fundo da caixa de textos

--- Jogo/src/cenas/Inicio.java
+++ Jogo/src/cenas/Inicio.java
@@ -2,6 +2,6 @@ package cenas;
 import graficos.Texto;
 public class Inicio {
 	public Inicio() {
-		new Texto("<html>janela de texto inicial</html>");//teste da classe Texto
+		new Texto("<html>Janela de texto inicial</html>");//teste da classe Texto
 	}
 }
--- Jogo/src/graficos/Texto.java
+++ Jogo/src/graficos/Texto.java
@@ -1,5 +1,9 @@
 package graficos;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
@@ -10,19 +14,30 @@ public class Texto extends Janela {
 	Janela texto = new Janela();
 	JButton continuar = new JButton("Continuar");
 	JLabel txt = new JLabel();
+	JLabel fundo = new JLabel(new ImageIcon("src/imagens/fundo_dialogo.png"));
 	
 	public Texto(String text) {
 		//criação da janela de texto
 		this.texto.construirJanela();
-		this.texto.setSize(600,400);
-		this.texto.add(continuar);
-		this.continuar.setBounds(460,320,100,30);
-		this.texto.setVisible(true);
+		this.texto.setSize(610,438);
 		
+		//botão continuar
+		this.texto.add(continuar);
+		this.continuar.setLayout(null);
+		this.continuar.setVisible(true);
+		this.continuar.setBounds(480,358,100,30);
 		//definição do texto da janela
 		this.txt.setText(text);
-		this.txt.setBounds(20,20,text.length()*10,30);
+		this.txt.setBounds(50,52,text.length()*10,30);
+		this.txt.setFont(new Font("Georgia",Font.BOLD, 25));
+		this.txt.setForeground(Color.black);
 		this.texto.add(txt);
+		
+		//fundo
+		this.texto.add(fundo);
+		this.fundo.setBounds(0,0,600,400);
+		
+		this.texto.setVisible(true);
 	}
 	
 	
