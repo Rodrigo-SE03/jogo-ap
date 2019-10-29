4b833964178eed635d848b84f57541fe5a9d6f9f
Rodrigo <sujeito300@gmail.com>
2019-10-29 20:17:37 -0300

    Começo

--- Jogo/src/cenas/Inicio.java
+++ Jogo/src/cenas/Inicio.java
@@ -2,9 +2,8 @@ package cenas;
 import graficos.Texto;
 public class Inicio {
 	public Inicio() {
-		new Texto("<html>Janela de texto inicial"
-				+ "<br/>com rima pra ficar bem legal"
-				+ "<br/>mais uma linha pra dar um grau"
-				+ "<br/>outra pra testar no final</html>",3);//teste da classe Texto
+		new Texto("<html>O dia amanheceu. Você acorda e se"
+				+ "<br/>levanta de sua cama"
+				+ "</html>",1);//teste da classe Texto
 	}
 }
