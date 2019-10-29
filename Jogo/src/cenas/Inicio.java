28bf26c6c1babf65caeb42065171ed79f8ae1547
Rodrigo <sujeito300@gmail.com>
2019-10-29 19:46:50 -0300

    Organização do início

--- Jogo/src/cenas/Inicio.java
+++ Jogo/src/cenas/Inicio.java
@@ -2,6 +2,9 @@ package cenas;
 import graficos.Texto;
 public class Inicio {
 	public Inicio() {
-		new Texto("<html>Janela de texto inicial<br/>com rima pra ficar bem legal<br/>mais uma linha pra dar um grau<br/>outra pra testar no final</html>",3);//teste da classe Texto
+		new Texto("<html>Janela de texto inicial"
+				+ "<br/>com rima pra ficar bem legal"
+				+ "<br/>mais uma linha pra dar um grau"
+				+ "<br/>outra pra testar no final</html>",3);//teste da classe Texto
 	}
 }
