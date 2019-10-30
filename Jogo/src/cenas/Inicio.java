7b1a58be17cf78386ab05660826474e2891dc952
pedro <ppaulo030601@gmail.com>
2019-10-29 21:26:49 -0300

    passa botão

--- Jogo/src/cenas/Inicio.java
+++ Jogo/src/cenas/Inicio.java
@@ -7,20 +7,35 @@ import menus.Menu_inicial;
 public class Inicio {
 	
 	Texto t1;
+	String[] tx = new String[5];
+	int[] linhas = new int[5];
 	
 	public Inicio() {
-		t1 = new Texto("<html>O dia amanheceu. Você acorda e se"
+		t1 = new Texto("<html>O dia amanheceu. Vocï¿½ acorda e se"
 				+ "<br/>levanta de sua cama"
 				+ "</html>",1);//teste da classe Texto
-	continuar();
+		tx[0] = "<html>O dia amanheceu. Vocï¿½ acorda e se</html>";
+		linhas[0] = 1;
+		tx[1] = "<html>levanta de sua cama</html>";
+		linhas[1]= 1;
+	continuar(t1,tx[0],linhas[0],5,0);
 	}
 	
-	public void continuar() {
-
-        t1.getContinuar().addActionListener( new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                new Menu_inicial();
-                t1.getTexto().dispose();
+	
+	
+	
+	public void continuar(Texto t,String txt, int x, int max,int cont) {
+		
+		int c;
+		Texto tex;
+		c = cont + 1;
+		
+		t.getContinuar().addActionListener( new ActionListener() {	
+			public void actionPerformed(ActionEvent e) {
+            	if(cont<=max) {
+            		continuar(new Texto(txt,x),tx[c],linhas[c],max,c);
+            	}
+            	else;
             } });
     }
 	
