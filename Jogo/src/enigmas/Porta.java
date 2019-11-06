0c2ff7e7e757a80e53a1d1f80ecd35ac22d99ba1
Rodrigo <sujeito300@gmail.com>
2019-11-06 20:43:36 -0300

    Pacote enigmas

---
+++ Jogo/src/enigmas/Porta.java
@@ -0,0 +1,55 @@
+package enigmas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+public class Porta {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	/*public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Porta window = new Porta();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}*/
+
+	/**
+	 * Create the application.
+	 */
+	public Porta() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 527, 426);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon(Porta.class.getResource("/imagens/senha.png")));
+		lblNewLabel.setBounds(0, 0, 513, 387);
+		panel.add(lblNewLabel);
+	}
+}
