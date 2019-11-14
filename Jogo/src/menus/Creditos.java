39e0729a5346a24f7f4615a0423e81453ec6f80a
Pedro <ppaulo030601@gmail.com>
2019-11-14 19:20:43 -0300

    Ajuste nos créditos

--- Jogo/src/menus/Creditos.java
+++ Jogo/src/menus/Creditos.java
@@ -1,11 +1,13 @@
 package menus;
 
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
 
-public class Creditos {
+public class Creditos extends JDialog {
 
 
-	JFrame tela = new JFrame("Creditos");
+	private static final long serialVersionUID = 1L;
 	JLabel rod = new JLabel(new ImageIcon("src/imagens/rod.png"));
 	JLabel pp = new JLabel(new ImageIcon("src/imagens/pp.png"));
 	JLabel rian = new JLabel(new ImageIcon("src/imagens/rian.png"));
@@ -14,19 +16,22 @@ public class Creditos {
 	JLabel fundo = new JLabel(new ImageIcon("src/imagens/pac_mano.gif"));
 
 	public void mostraCreditos() {
-		tela.add(rod);
-		tela.add(dan);
-		tela.add(mig);
-		tela.add(rian);
-		tela.add(pp);
-		tela.add(fundo);
-		tela.setSize(790, 525);
+		ImageIcon icone = new ImageIcon("src/imagens/Icone.png");
+		this.setIconImage(icone.getImage());
+		this.add(rod);
+		this.add(dan);
+		this.add(mig);
+		this.add(rian);
+		this.add(pp);
+		this.add(fundo);
+		this.setSize(790, 525);
 		dan.setBounds(500, 340, 264, 41);
 		mig.setBounds(500, 280, 264, 41);
 		rian.setBounds(500, 150, 264, 41);
 		pp.setBounds(424, 90, 340, 41);
 		rod.setBounds(500, 30, 264, 41);
-		tela.setVisible(true);
+		this.setModal(true);
+		this.setVisible(true);
 	}
 	
 
